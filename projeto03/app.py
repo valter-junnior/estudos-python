3e01999b2f8ad80de1d2e59f50c5f1e1c33e5bc4
Junnior <juniornunes1008@gmail.com>
2025-11-12 21:39:08 -0300

    add comentarios

--- projeto03/app.py
+++ projeto03/app.py
@@ -6,7 +6,7 @@ lojas = pd.read_csv("projeto03/data/lojas.csv")
 produtos = pd.read_csv("projeto03/data/produtos.csv")
 vendas = pd.read_csv("projeto03/data/vendas.csv")
 
-lojas["data_abertura"] = pd.to_datetime(lojas["data_abertura"])
+lojas["data_abertura"] = pd.to_datetime(lojas["data_abertura"]) # feito com ajuda no pd.to_datetime
 vendas["data_venda"] = pd.to_datetime(vendas["data_venda"])
 vendas["valor_total"] = vendas["quantidade"] * vendas["valor_unitario"]
 
@@ -21,7 +21,7 @@ df = pd.merge(df, produtos)
 
 print(df[["nome_loja", "cidade", "nome_produto", "categoria", "quantidade", "valor_total", "data_venda"]], "\n")
 
-faturamento_mensal_por_loja = df.groupby(["nome_loja", df["data_venda"].dt.month])["valor_total"].sum()
+faturamento_mensal_por_loja = df.groupby(["nome_loja", df["data_venda"].dt.month])["valor_total"].sum() # feito com ajuda no df["data_venda"].dt.month
 faturament_total_por_categoria_de_produto = df.groupby("categoria")["valor_total"].sum()
 
 print(faturamento_mensal_por_loja, "\n")
@@ -38,7 +38,7 @@ opcoes = [
     df["margem_lucro"] <= df["valor_unitario"] * 0.3
 ]
 categorias = ["alto", "baixo"]
-df["desempenho"] = np.select(opcoes, categorias, default="neutro")
+df["desempenho"] = np.select(opcoes, categorias, default="neutro") # feito com ajuda no default="neutro
 
 """
 df["desempenho"] = np.where(
@@ -50,11 +50,12 @@ df["desempenho"] = np.where(
 
 print(df[["valor_unitario", "margem_lucro", "desempenho"]],  "\n")
 
-lojas_maior_faturamento = df.groupby(["nome_loja"])["valor_total"].sum().sort_values(ascending=False)[:5] # usar reset_index e o  .head(5)
+lojas_maior_faturamento = df.groupby(["nome_loja"])["valor_total"].sum().sort_values(ascending=False)[:5] # usar reset_index e o  .head(5)  # feito com ajuda no sort_values(ascending=False)
 produtos_mais_vendidos_quantidade = df.groupby("nome_produto")["quantidade"].sum().sort_values(ascending=False)[:3]
 print(lojas_maior_faturamento, "\n")
 print(produtos_mais_vendidos_quantidade, "\n")
 
+# todo com ajuda
 hoje = datetime.now()
 lojas["dias_operacao"] = (hoje - lojas["data_abertura"]).dt.days
 print(lojas[["nome_loja", "dias_operacao"]])
